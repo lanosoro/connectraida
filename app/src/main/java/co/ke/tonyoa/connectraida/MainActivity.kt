ef608204bce86a0fbd034bbddae0e70aa5fb447e
Ianosoro <43702447+lanosoro@users.noreply.github.com>
2022-12-30 05:40:14 +0300

    view binding added

--- app/src/main/java/co/ke/tonyoa/connectraida/MainActivity.kt
+++ app/src/main/java/co/ke/tonyoa/connectraida/MainActivity.kt
@@ -2,10 +2,19 @@ package co.ke.tonyoa.connectraida
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import co.ke.tonyoa.connectraida.databinding.ActivityMainBinding
+import co.ke.tonyoa.connectraida.databinding.BottomsheetFragBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val bottomSheetFragment  = BottomSheetFragment()
+        binding.signup.setOnClickListener { bottomSheetFragment.show(supportFragmentManager,"Signup bottomsheet") }
+
+        }
     }
-}
