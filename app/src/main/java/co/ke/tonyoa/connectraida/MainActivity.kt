7efb5dcddd33a68f6cc2733f83f515ea0ebdbeca
Ianosoro <43702447+lanosoro@users.noreply.github.com>
2022-12-31 03:49:30 +0300

    routed

--- app/src/main/java/co/ke/tonyoa/connectraida/MainActivity.kt
+++ app/src/main/java/co/ke/tonyoa/connectraida/MainActivity.kt
@@ -1,20 +1,28 @@
 package co.ke.tonyoa.connectraida
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import co.LoginActivity
 import co.ke.tonyoa.connectraida.databinding.ActivityMainBinding
 import co.ke.tonyoa.connectraida.databinding.BottomsheetFragBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
         val bottomSheetFragment  = BottomSheetFragment()
         binding.signup.setOnClickListener { bottomSheetFragment.show(supportFragmentManager,"Signup bottomsheet") }
-        binding.login.setOnClickListener {  }
+        binding.login.setOnClickListener {
+            val Intent = Intent(this,LoginActivity::class.java)
+            startActivity(Intent)
+        }
+
         }
     }
