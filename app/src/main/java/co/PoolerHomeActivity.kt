a4b7872c50c875f677d590a26f986a3ec1a546d8
Ianosoro <43702447+lanosoro@users.noreply.github.com>
2023-01-16 23:20:17 +0300

    multidex fix

---
+++ app/src/main/java/co/PoolerHomeActivity.kt
@@ -0,0 +1,12 @@
+package co
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import co.ke.tonyoa.connectraida.R
+
+class PoolerHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pooler_home)
+    }
+}
